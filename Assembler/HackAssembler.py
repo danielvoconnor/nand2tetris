b9509fc894df38281acc9c976377cc0f6f7a84c6
Daniel O'Connor <daniel.v.oconnor@gmail.com>
2021-09-08 17:18:40 -0700

    added code to read in a text file

--- Assembler/HackAssembler.py
+++ Assembler/HackAssembler.py
@@ -1,10 +1,16 @@
 
 print('Hello world!!')
 
+filename = 'Add.asm'
+
+with open(filename) as file:
+    lines = file.readlines()
+    lines = [line.rstrip() for line in lines]
 
 
+for line in lines:
 
-
+    print(line)
 
 
 
