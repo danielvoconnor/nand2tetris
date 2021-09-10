d22ea1126fd17b9971ff38838379092dd768824a
Daniel O'Connor <daniel.v.oconnor@gmail.com>
2021-09-09 20:32:43 -0700

    work in progress

--- Assembler/HackAssembler.py
+++ Assembler/HackAssembler.py
@@ -1,16 +1,21 @@
 
-print('Hello world!!')
-
 filename = 'Add.asm'
 
 with open(filename) as file:
     lines = file.readlines()
-    lines = [line.rstrip() for line in lines]
+    lines = [line.rstrip().lstrip() for line in lines]
 
-
+count = 0
 for line in lines:
 
+    line = line.partition('//')[0]
+    if not line:
+        continue
+
+    
+
     print(line)
+    count += 1
 
 
 
