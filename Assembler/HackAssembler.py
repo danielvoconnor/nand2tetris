d25645caf14dd20b4f56ae8897144084df1e41a1
Daniel O'Connor <daniel.v.oconnor@gmail.com>
2021-09-09 23:57:57 -0700

    added translate_comp function

--- Assembler/HackAssembler.py
+++ Assembler/HackAssembler.py
@@ -29,6 +29,68 @@ def translate_dest(dest):
     bits = ''.join(bits)
     return bits
 
+def translate_comp(comp):
+
+    if comp == '0':
+        return '0101010'
+    elif comp == '1':
+        return '0111111'
+    elif comp == '-1':
+        return '0111010'
+    elif comp == 'D':
+        return '0001100'
+    elif comp == 'A':
+        return '0110000'
+    elif comp == 'M':
+        return '1110000'
+    elif comp == '!D':
+        return '0001101'
+    elif comp == '!A':
+        return '0110001'
+    elif comp == '!M':
+        return '1110001'
+    elif comp == '-D':
+        return '0001111'
+    elif comp == '-A':
+        return '0110011'
+    elif comp == '-M':
+        return '1110011'
+    elif comp == 'D+1':
+        return '0011111'
+    elif comp == 'A+1':
+        return '0110111'
+    elif comp == 'M+1':
+        return '1110111'
+    elif comp == 'D-1':
+        return '0001110'
+    elif comp == 'A-1':
+        return '0110010'
+    elif comp == 'M-1':
+        return '1110010'
+    elif comp == 'D+A':
+        return '0000010'
+    elif comp == 'D+M':
+        return '1000010'
+    elif comp == 'D-A':
+        return '0010011'
+    elif comp == 'D-M':
+        return '1010011'
+    elif comp == 'A-D':
+        return '0000111'
+    elif comp == 'M-D':
+        return '1000111'
+    elif comp == 'D&A':
+        return '0000000'
+    elif comp == 'D&M':
+        return '1000000'
+    elif comp == 'D|A':
+        return '0010101'
+    elif comp == 'D|M':
+        return '1010101'
+    else:
+        print('ERROR: COMP NOT RECOGNIZED')
+        return -1
+
 def translate_jump(jump):
 
     if jump == '': 
@@ -88,8 +150,8 @@ for line in lines:
             
         print('Full line: ', line)
         print('dest: ', dest, '(', translate_dest(dest),')')
-        print('comp: ', comp)
-        print('jump: ', jump)
+        print('comp: ', comp, '(', translate_comp(comp), ')')
+        print('jump: ', jump, '(', translate_jump(jump), ')')
         print('\n')
 
 
