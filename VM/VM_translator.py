d4c73b05a453e281a84e7b2db0381337cf50bc26
Daniel O'Connor <daniel.v.oconnor@gmail.com>
2021-09-19 18:51:54 -0700

    Creating write_push function

--- VM/VM_translator.py
+++ VM/VM_translator.py
@@ -1,7 +1,42 @@
-print('hello world')
+import sys
 
 
+def write_push(segment, i):
 
+    if segment == 'constant':
+        return 0
+    else:
+        return ['//push ' + segment + ' ' + str(i),
+                '@'+str(i),
+                'D=A',
+                '@'+segment,
+                'A=M+D',
+                'D=M',
+                '@SP',
+                'A=M',
+                'M=D',
+                '@SP',
+                'M=M+1']
+
+
+fname = sys.argv[1]
+print('filename: ', fname)
+f = open(fname)
+lines_raw = f.readlines()
+f.close()
+
+# Remove comments and all whitespace
+lines_trimmed = []
+for line in lines_raw:
+
+    line = line.partition('//')[0]
+    if line.strip():
+        lines_trimmed.append(line)
+
+
+for line in lines_trimmed:
+
+    print(line.split())
 
 
 
