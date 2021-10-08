64fdf3d514a21e92f8938f9fb4e1a97271bcf03f
Daniel O'Connor <daniel.v.oconnor@gmail.com>
2021-10-08 01:50:58 -0700

    wrote draft of write_call

--- VM/VM_translator.py
+++ VM/VM_translator.py
@@ -108,58 +108,86 @@ def write_if(lbl,fun_name):
 
     return commands
 
+def write_call(fun_name, num_args,i):
+    # To understand what i is, see p. 163 of The Elements of Computing Systems:
+    # "The handling of each call command within foo's code generates, and
+    # injects into the assembly code stream, a symbol XXX.foo@ret.i, where i is
+    # a running integer (one such symbol is generated for each call command within foo).
+
+    return_label = fun_name + '$ret.' + str(i)
+    push_return_addr = ['@' + return_label,'D=A'] + push_D
+    push_LCL = ['@LCL','D=M'] + push_D
+    push_ARG = ['@ARG','D=M'] + push_D
+    push_THIS = ['@THIS','D=M'] + push_D
+    push_THAT = ['@THAT','D=M'] + push_D
+    reposition_ARG = ['@SP','D=M','@5','D=D-A','@' + str(num_args),'D=D-A'] + push_D
+    reposition_LCL = ['@SP','D=M','@LCL','M=D']
+    goto_f = ['@' + fun_name,'0;JMP']
+    place_return_label = ['(' + return_label + ')']
+
+    commands = push_return_addr + push_LCL + push_ARG + push_THIS + push_THAT + reposition_ARG \
+               + reposition_LCL + goto_f + place_return_label
+
+    return commands
+
+def write_function(fun_name, num_vars):
+
+    commands = ['(' + fun_name + ')']
+
+    return commands
+
 ############################# Now do the translation. #####################
+if False:
+    # Read in the code
+    fname = sys.argv[1] # fname may or may not contain a full path.
+    print('filename: ', fname)
+    f = open(fname)
+    lines_raw = f.readlines()
+    f.close()
+    fname_without_path = fname.split('/')[-1][:-3] # '/Users/dvo/foo.VM' becomes 'foo', for example.
 
-# Read in the code
-fname = sys.argv[1] # fname may or may not contain a full path.
-print('filename: ', fname)
-f = open(fname)
-lines_raw = f.readlines()
-f.close()
-fname_without_path = fname.split('/')[-1][:-3] # '/Users/dvo/foo.VM' becomes 'foo', for example.
+    # Remove comments and all whitespace
+    lines_trimmed = []
+    for line in lines_raw:
 
-# Remove comments and all whitespace
-lines_trimmed = []
-for line in lines_raw:
+        line = line.partition('//')[0]
+        if line.strip():
+            lines_trimmed.append(line)
 
-    line = line.partition('//')[0]
-    if line.strip():
-        lines_trimmed.append(line)
+    fun_name = 'Main.main'
+    hack_code = initialize_SP
+    for line in lines_trimmed:
 
-fun_name = 'Main.main'
-hack_code = initialize_SP
-for line in lines_trimmed:
+        words = line.split()
+        if words[0] == 'push':
+            segment = words[1]
+            i = words[2]
+            hack_code = hack_code + write_push(segment,i)
+        elif words[0] == 'pop':
+            segment = words[1]
+            i = words[2]
+            hack_code = hack_code + write_pop(segment,i)
+        elif words[0] in ['add','sub','neg','eq','gt','lt','and','or','not']:
+            hack_code = hack_code + write_arithmetic(words[0])
+        elif words[0] == 'label':
+            lbl = words[1]
+            hack_code = hack_code + write_label(lbl,fun_name)
+        elif words[0] == 'goto':
+            lbl = words[1]
+            hack_code = hack_code + write_goto(lbl,fun_name)
+        elif words[0] == 'if-goto':
+            lbl = words[1]
+            hack_code = hack_code + write_if(lbl,fun_name)
+        else:
+            print('ERROR: words[0] not recognized!')
 
-    words = line.split()
-    if words[0] == 'push':
-        segment = words[1]
-        i = words[2]
-        hack_code = hack_code + write_push(segment,i)
-    elif words[0] == 'pop':
-        segment = words[1]
-        i = words[2]
-        hack_code = hack_code + write_pop(segment,i)
-    elif words[0] in ['add','sub','neg','eq','gt','lt','and','or','not']:
-        hack_code = hack_code + write_arithmetic(words[0])
-    elif words[0] == 'label':
-        lbl = words[1]
-        hack_code = hack_code + write_label(lbl,fun_name)
-    elif words[0] == 'goto':
-        lbl = words[1]
-        hack_code = hack_code + write_goto(lbl,fun_name)
-    elif words[0] == 'if-goto':
-        lbl = words[1]
-        hack_code = hack_code + write_if(lbl,fun_name)
-    else:
-        print('ERROR: words[0] not recognized!')
-
-hack_code = hack_code + infinite_loop
-hack_code = [s + '\n' for s in hack_code]
-fname_out = fname[:-3] + '.asm'
-print('fname_out: ', fname_out)
-target = open(fname_out,'w')
-target.writelines(hack_code)
-target.close()
+    hack_code = hack_code + infinite_loop
+    hack_code = [s + '\n' for s in hack_code]
+    fname_out = fname[:-3] + '.asm'
+    print('fname_out: ', fname_out)
+    target = open(fname_out,'w')
+    target.writelines(hack_code)
+    target.close()
 
 
 
