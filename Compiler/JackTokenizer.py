120bc01c198e61ee48cc0ea88e279c68198c5124
Daniel O'Connor <daniel.v.oconnor@gmail.com>
2021-10-26 23:10:03 -0700

    working on extract_token function

--- Compiler/JackTokenizer.py
+++ Compiler/JackTokenizer.py
@@ -1,41 +1,92 @@
 import sys
 
-# Read the code to be tokenized into a string.
-fname = sys.argv[1]
-f = open(fname)
-code = f.read()
-f.close()
+# Now tokenize the code
+symbols = ['{','}','(',')','[',']','.',',',';','+','-','*','/','&','|','<','>','=','~']
 
-# Remove /* ... */ style comments from the code
-code_without_comments = ''
-tail = code
-while tail != '':
+def extract_token(code):
+    # code is a string containing the contents of a .jack file
+    # I'll assume that comments have already been removed.
+    # I'll also assume code is not pure whitespace.
 
-    parts = tail.partition('/*')
-    code_without_comments = code_without_comments + parts[0]
-    tail = parts[2].partition('*/')[2]
+    code = code.strip()
+    if len(code) == 1:
+        return code, ''
 
-code = code_without_comments
+    if code[0] == '"':
+        parts = code[1:].partition('"')
+        token = parts[0]
+        code = parts[2]
+        return token, code
 
-# Remove // style comments from the code
-code_without_comments = ''
-tail = code
-while tail != '':
+    if code[0] in symbols:
 
-    parts = tail.partition('//')
-    code_without_comments = code_without_comments + parts[0]
-    tail = parts[2]
+        token = code[0]
+        code = code[1:]
+        return token, code
 
-    parts = tail.partition('\n')
-    tail = parts[1] + parts[2]
+    if code[0].isdigit():
 
-code = code_without_comments
+        token = ''
+        i = 0
+        while code[i].isdigit():
+            token = token + code[i]
+            i = i + 1
+        code = code[i:]
+        return token, code
+
+    token = ''
+    i = 0
+    while (code[i] not in symbols) and (not code[i].isspace()):
+        token = token + code[i]
+        i = i + 1
+
+    code = code[i:]
+    return token, code
+
+    print('ERROR: TOKEN EXTRACTION FAILED')
+    sys.exit()
 
 
 
-check_file = open('code_without_comments.jack','w')
-check_file.write(code_without_comments)
-check_file.close()
+if True:
+    # Read the code to be tokenized into a string.
+    fname = sys.argv[1]
+    f = open(fname)
+    code = f.read()
+    f.close()
+
+    # Remove /* ... */ style comments from the code
+    code_without_comments = ''
+    tail = code
+    while tail != '':
+
+        parts = tail.partition('/*')
+        code_without_comments = code_without_comments + parts[0]
+        tail = parts[2].partition('*/')[2]
+
+    code = code_without_comments
+
+    # Remove // style comments from the code
+    code_without_comments = ''
+    tail = code
+    while tail != '':
+
+        parts = tail.partition('//')
+        code_without_comments = code_without_comments + parts[0]
+        tail = parts[2]
+
+        parts = tail.partition('\n')
+        tail = parts[1] + parts[2]
+
+    code = code_without_comments
+
+
+
+    check_file = open('code_without_comments.jack','w')
+    check_file.write(code_without_comments)
+    check_file.close()
+
+    tokens = []
 
 
 
@@ -44,6 +95,20 @@ check_file.close()
 
 
 
-tokens = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
