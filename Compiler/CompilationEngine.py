9e5c7db9f574b88fc049299d2551b0d1c0c2a0a7
Daniel O'Connor <daniel.v.oconnor@gmail.com>
2021-11-07 17:07:43 -0800

    finished draft of compile_parameterList function

--- Compiler/CompilationEngine.py
+++ Compiler/CompilationEngine.py
@@ -1,73 +1,102 @@
+def xml_for_type(token):
 
+    if token in ['int','char','boolean']:
+        xml = '<keyword> ' + token + ' </keyword>'
+    else:
+        xml = '<identifier> ' + token + ' </identifier>'
 
-def compile_class(tokens,token_types):
+    return xml
+   
 
-    xml = ['<class>']
-    xml_className, tokens, token_types = compile_className(tokens[1:],token_types[1:])
-    xml = xml + xml_className
+def compile_class(tokens):
 
+    xml = ['<class>','<keyword> class </keyword>']
+    xml = xml + ['<identifier> ' + tokens[1] + ' </identifier>']
     xml = xml + ['<symbol> { </symbol>']
-    tokens = tokens[1:]
-    token_types = token_types[1:]
+    tokens = tokens[3:]
 
     while tokens[0] in ['static','field']:
 
-        xml_classVarDec, tokens, token_types = compile_classVarDec(tokens,token_types)
+        xml_classVarDec, tokens = compile_classVarDec(tokens)
         xml = xml + xml_classVarDec
 
     while tokens[0] in ['constructor','function','method']:
 
-        xml_subroutineDec, tokens, token_types = compile_subroutineDec(tokens,token_types)
+        xml_subroutineDec, tokens = compile_subroutineDec(tokens)
         xml = xml + xml_subroutineDec
 
     xml = xml + ['<symbol> } </symbol>', '</class>']
 
     return xml
 
-def compile_className(tokens,token_types):
-
-    xml = ['<className>','<identifier> ' + tokens[0] + ' </identifier>', '</className>']
-    return xml, tokens[1:], token_types[1:]
-
-def compile_classVarDec(tokens,token_types):
-    # PICK UP HERE. I NEED A compile_type FUNCTION. 
-    # EACH NONTERMINAL ELEMENT NEEDS ITS OWN OPENING AND CLOSING TAGS.
-    # BE CONSISTENT ABOUT THAT.
-    xml = ['<classVarDec>', '<keyword> ' + tokens[0] + ' </keyword>', \
-           '<type> ' + tokens[1] + ' </type>','<varName> ' + tokens[2] + ' </varName>']
+def compile_classVarDec(tokens):
+    xml = ['<classVarDec>', '<keyword> ' + tokens[0] + ' </keyword>']
+    xml = xml + [xml_for_type(tokens[1]),'<identifier> ' + tokens[2] + ' </identifier>']
 
     i = 3
     while tokens[i] == ',':
-        xml = xml + ['<symbol> , </symbol>', '<varName> ' + tokens[i+1] + ' </varName>']
+        xml = xml + ['<symbol> , </symbol>', '<identifier> ' + tokens[i+1] + ' </identifier>']
         i = i+2
 
     xml = xml + ['<symbol> ; </symbol>', '</classVarDec>']
     tokens = tokens[i+1:]
-    token_types = token_types[i+1,:]
 
-    return xml, tokens, token_types
+    return xml, tokens
 
-def compile_subroutineDec(tokens,token_types):
+def compile_subroutineDec(tokens):
 
     xml = ['<subroutineDec>', '<keyword> ' + tokens[0] + '</keyword>']
     if tokens[1] == 'void':
         xml = xml + ['<keyword> void </keyword>']
     else:
-        xml = xml + ['<type> ' + tokens[1] + ' </type>']
+        xml = xml + ['<identifier> ' + tokens[1] + ' </identifier>']
 
-    xml = xml + ['<subroutineName> ' + tokens[2] + ' </subroutineName>','<symbol> ( </symbol>']
+    xml = xml + ['<identifier> ' + tokens[2] + ' </identifier>','<symbol> ( </symbol>']
     tokens = tokens[4:]
-    token_types = token_types[4:]
 
-    xml_parameterList, tokens, token_types =  compile_parameterList(tokens,token_types) 
+    xml_parameterList, tokens =  compile_parameterList(tokens) 
     xml = xml + xml_parameterList + ['<symbol> ) </symbol>']
-    xml_subroutineBody, tokens, token_types = compile_subroutineBody(tokens[1:],token_types[1:])
+    xml_subroutineBody, tokens = compile_subroutineBody(tokens[1:])
     xml = xml + xml_subroutineBody
 
-    return xml, tokens, token_types
+    return xml, tokens
+
+def compile_parameterList(tokens):
+
+    if tokens[0] != ')':
+
+        xml = [xml_for_type(tokens[0]),'<identifier> ' + tokens[1] + ' </identifier>']
+        i = 2
+        while tokens[i] == ',':
+            xml = xml + ['<symbol> , </symbol>',xml_for_type(tokens[i+1])]
+            i = i + 2
+        tokens = tokens[i:]
+        return xml, tokens
+
+    else:
+        xml = []
+        return xml, tokens
+
+# PICK UP HERE. WRITE compile_subroutineBody, ETC.
 
 
-    # PICK UP HERE. WRITE compile_parameterList and compile_subroutineBody.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
